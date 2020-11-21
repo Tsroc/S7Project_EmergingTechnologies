dc8ac156ef742d8114b786d7e2258f3d4fe19e11
Eoin Wilkie <G00363134@gmit.ie>
2020-11-21 16:40:11 +0000

    Predicts random data from webserver, sepertated into linear regression and

--- WebServer/Test.py
+++ WebServer/Test.py
@@ -2,6 +2,7 @@
 import flask as fl
 # numpy for numerical work.
 import numpy as np
+import random
 
 # Create a new web app.
 app = fl.Flask(__name__)
@@ -21,3 +22,10 @@ def uniform():
 @app.route('/api/normal')
 def normal():
     return{"value":np.random.normal()}
+
+
+# Add normal route.
+@app.route('/api/windspeed')
+def windspeed():
+    value = random.randint(0, 25)
+    return{"value":value}
